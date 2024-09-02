072afb1034ba573a4cea41eaddef09b12cb50c02
kpfuller28 <kpfuller28@gmail.com>
2024-09-02 13:09:57 -0400

    part 2 complete

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,18 @@
 
 class Book:
+    author = ''
+    title = ''
     def __init__(self, author, title):
-        pass
-    
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        print(f'{self.title}, written by {self.author}')
 
 
 if __name__ == "__main__":
-    pass
+    harry_potter = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    ivanhoe = Book("Walter Scott", "Ivanhoe: A Romance")
+
+    harry_potter.display()
+    ivanhoe.display()
